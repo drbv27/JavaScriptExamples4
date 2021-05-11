3f1ba9777fb3f3ab731af06a3add518000d57688
Diego Bonilla <drbv27@gmail.com>
2021-05-11 10:42:04 -0500

    Several Solutions to Excercise 12

--- assets/app.js
+++ assets/app.js
@@ -11,8 +11,40 @@
 //E12S1.Solucion Simple:
 console.log("-----------------------------------------------------------------------------------")
 console.log("Ejercicio 12:")
+function primo (num) {
+    let cont=0;
+    for (let i = 1; i<=num; i++) {
+        if(num%i===0){
+            cont++
+        }
+    }
+    (cont===2)
+    ?console.log('el numero es primo')
+    :console.log('el numero no es primo')
+}
+primo(12)
 
+//E12S2 Arrow y Op.Ternario
 
+const prime = num =>{
+    let cont=0;
+    for (let i = 1; i<=num; i++) if(num%i===0)cont++;
+    (cont===2)
+    ?console.log(`El numero "${num}" es Primo`)
+    :console.log(`El numero "${num}" NO es Primo`)
+}
+prime(8)
+
+//E12S3 similar with validadion
+const prim = (num=undefined) =>{
+    if(num===undefined) console.warn('No digitaste un número.')
+    let cont=0;
+    for (let i = 1; i<=num; i++) if(num%i===0)cont++;
+    (cont===2)
+    ?console.log(`El numero "${num}" es Primo`)
+    :console.log(`El numero "${num}" NO es Primo`)
+}
+prim(11)
 
 //--------------------------------------------------------------------------------------------
 
