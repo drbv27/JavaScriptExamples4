a6b19c74fed84d9c56f0e885b4a10c1dac177451
Diego Bonilla <drbv27@gmail.com>
2021-05-11 08:31:15 -0500

    Initial Commit

---
+++ assets/app.js
@@ -0,0 +1,29 @@
+/*Ejercicios Basicos JS Nivel 1
+    12.Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no.Ejemplo: miFuncion(7) devolverá "true".
+    13.Programa una función que determine si un número es par o impar.Ejemplo: miFuncion(29) devolverá Impar.
+    14.Programa una función para convertir grados Celsius a Fahrenheit y viceversa.Ejemplo: miFuncion(0,"C") devolverá 32°F.
+*/
+
+//Soluciones:
+
+//--------------------------------------------------------------------------------------------
+
+//E12S1.Solucion Simple:
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 12:")
+
+
+
+//--------------------------------------------------------------------------------------------
+
+//E13S1.Solución Simple
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 13:")
+
+
+
+//--------------------------------------------------------------------------------------------
+
+//E14S1.Solución Simple
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 14:")
